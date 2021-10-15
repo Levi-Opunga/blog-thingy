1f4027b576a2c691d2f1c195f0a520b9d1272b29
Levi-Opunga <leviopunga@gmail.com>
2021-10-16 01:43:04 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FirstComponent } from './articles/first/first.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SecondComponent } from './articles/second/second.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    FirstComponent,
+    NavbarComponent,
+    SecondComponent
   ],
   imports: [
     BrowserModule,
